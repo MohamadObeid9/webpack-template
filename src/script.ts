17a50b026be90e98a6d69c0b96dda7e8c2540a8f
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-12-29 16:42:41 +0200

    added eslint and prettier

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,6 @@
-export  const createPara = ()=>{
-    const para = document.createElement('p');
-    para.textContent= "Hello World";
-    para.style.cssText="font-weight:bold;font-size:40px";
-    document.body.appendChild(para);
-}
+export const createPara = () => {
+  const para = document.createElement("p");
+  para.textContent = "Hello World";
+  para.style.cssText = "font-weight:bold;font-size:40px";
+  document.body.appendChild(para);
+};

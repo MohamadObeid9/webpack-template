4d7148e943b876170cc37d6027c1c80b14d2d258
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2025-01-08 19:42:17 +0200

    testing gh-pages

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,6 @@
 export const createPara = () => {
   const para = document.createElement("p");
-  para.textContent = "Hello World";
+  para.textContent = "Hello odin";
   para.style.cssText = "font-weight:bold;font-size:40px";
   document.body.appendChild(para);
 };

e41ed48c46493b43c98caba437bcfe65ba232a66
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2025-01-12 16:15:45 +0200

    test2

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,6 +1,6 @@
 const createPara = () => {
   const para = document.createElement("p");
-  para.textContent = "Hello odin";
+  para.textContent = "Hello World";
   para.classList.add("text-red-600", "font-bold", "text-4xl","dark:text-amber-400");
   document.body.appendChild(para);
 };
